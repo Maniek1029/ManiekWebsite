e8dc72639da0c235488f41ca2f4d1a144fef54ac
Maniek <61874757+Maniek1029@users.noreply.github.com>
2024-04-29 21:00:12 +0200

    update strony 2.0

--- script/scroll.js
+++ script/scroll.js
@@ -1,10 +1,10 @@
-var nav = document.querySelector('nav');
+// var nav = document.querySelector('nav');
 
-window.addEventListener('scroll', function(){
-  if(window.pageYOffset > 100) {
-    nav.style.backgroundColor = "rgba(0, 0, 0, 0.9)"
-  }
-  else {
-    nav.style.backgroundColor = "rgba(0, 0, 0, 0.5)"
-  }
-})
+// window.addEventListener('scroll', function(){
+//   if(window.pageYOffset > 100) {
+//     nav.style.backgroundColor = "rgba(255, 255, 255, 1)"
+//   }
+//   else {
+//     nav.style.backgroundColor = "rgba(255, 255, 255, 0.5)"
+//   }
+// })
